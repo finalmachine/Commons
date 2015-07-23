b36681e910a53beb6080225eeef9784aad002970
finalmachine <finalmachine@163.com>
2015-07-24 07:52:02 +0800

    edit MsgConsumers

--- src/main/java/com/gbi/commons/net/amqp/MsgConsumers.java
+++ src/main/java/com/gbi/commons/net/amqp/MsgConsumers.java
@@ -1,5 +1,23 @@
 package com.gbi.commons.net.amqp;
 
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 public class MsgConsumers {
-	
+
+	private static int consumersNum = 8;
+	private static int MAX_QUEUQ_SIZE = 100;
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ExecutorService es = new ThreadPoolExecutor(consumersNum, consumersNum, 10000L, TimeUnit.MILLISECONDS,
+				new ArrayBlockingQueue<Runnable>(MAX_QUEUQ_SIZE), new ThreadPoolExecutor.CallerRunsPolicy());
+		Connection conn = new ConnectionFactory().newConnection(es);
+	}
 }
