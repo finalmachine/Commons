45b480ad25b5efda2867ae204789514824396e21
leo.li <leo.li@generalbiologic.com>
2015-07-23 09:45:34 +0800

    nothing

--- src/main/java/com/gbi/commons/util/file/Excel2007Writer.java
+++ src/main/java/com/gbi/commons/util/file/Excel2007Writer.java
@@ -12,24 +12,28 @@ import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFDataFormat;
 
 public class Excel2007Writer {
 	public static void main(String[] args) throws IOException {
-		Workbook book = new SXSSFWorkbook(100);
-		Sheet sheet = book.createSheet("表单1");
+		SXSSFWorkbook book = new SXSSFWorkbook(100);
+		SXSSFSheet sheet = (SXSSFSheet) book.createSheet("表单1");
 		
-		CellStyle dateStyle = book.createCellStyle();
-		DataFormat dateFormat = book.createDataFormat();
+		
+		XSSFDataFormat dateFormat = (XSSFDataFormat) book.createDataFormat();
 		System.out.println(HSSFDataFormat.getBuiltinFormat("m/d/yy"));
 	//	dateStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("m/d/yy"));
-		dateStyle.setDataFormat(dateFormat.getFormat("2001/3/7"));
+	//	dateStyle.setDataFormat((short)31);//dateFormat.getFormat("2001/3/7")
 
-		for (int rownum = 0; rownum < 1000; ++rownum) {
-			Row row = sheet.createRow(rownum);
-			for (int cellnum = 0; cellnum < 20; cellnum++) {
-				Cell cell = row.createCell(cellnum);
-				if (cellnum % 2 == 0) {
+		for (int rownum = 0; rownum < 60; ++rownum) {
+			SXSSFRow row = (SXSSFRow) sheet.createRow(rownum);
+				SXSSFCell cell = (SXSSFCell) row.createCell(0);
+				/*if (cellnum % 2 == 0) {
 					cell.setCellType(Cell.CELL_TYPE_NUMERIC);
 					cell.setCellValue(RandomUtils.nextInt(0, 1000));
 				} else if (cellnum % 3 == 0) {
@@ -39,10 +43,14 @@ public class Excel2007Writer {
 				} else {
 					cell.setCellType(Cell.CELL_TYPE_STRING);
 					cell.setCellValue("abcd");
-				}
-			}
+				}*/
+				cell.setCellType(Cell.CELL_TYPE_NUMERIC);
+				XSSFCellStyle dateStyle = (XSSFCellStyle) book.createCellStyle();
+				dateStyle.setDataFormat((short)rownum);
+				cell.setCellStyle(dateStyle);
+				cell.setCellValue(Calendar.getInstance().getTime());
 		}
-		FileOutputStream out = new FileOutputStream("D:\\test2.xlsx");
+		FileOutputStream out = new FileOutputStream("D:\\test3.xlsx");
 		book.write(out);
 		out.close();
 		book.close();
