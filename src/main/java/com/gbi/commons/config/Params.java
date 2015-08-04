77e451d79843abbb767769cd81527f27f0070bfa
finalmachine <finalmachine@163.com>
2015-08-05 07:46:51 +0800

    params

--- src/main/java/com/gbi/commons/config/Params.java
+++ src/main/java/com/gbi/commons/config/Params.java
@@ -3,8 +3,8 @@ package com.gbi.commons.config;
 public class Params {
 	public enum MongoDB {
 		NAVISUS("127.0.0.1", 27017, "NAVISUS"),
-		PROXIES("127.0.0.1", 27017, "PROXIES");
-		
+		PROXIES("127.0.0.1", 27017, "PROXIES"),
+		TEST("127.0.0.1", 27017, "TEST");
 		public String host;
 		public int port;
 		public String database;
